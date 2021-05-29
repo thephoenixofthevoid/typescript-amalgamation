6554447d86f45578080bdf7dae7d6fa900989d43
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2021-05-29 06:41:50 +0300

    Core module

---
+++ src/modules/core.ts
@@ -0,0 +1,46 @@
+/**
+ * Gets the actual offset into an array for a relative offset. Negative offsets indicate a
+ * position offset from the end of the array.
+ */
+function toOffset(array: readonly any[], offset: number) {
+    return offset < 0 ? array.length + offset : offset;
+}
+
+/**
+ * Appends a range of value to an array, returning the array.
+ *
+ * @param to The array to which `value` is to be appended. If `to` is `undefined`, a new array
+ * is created if `value` was appended.
+ * @param from The values to append to the array. If `from` is `undefined`, nothing is
+ * appended. If an element of `from` is `undefined`, that element is not appended.
+ * @param start The offset in `from` at which to start copying values.
+ * @param end The offset in `from` at which to stop copying values (non-inclusive).
+ */
+export function addRange<T>(to: T[], from: readonly T[] | undefined, start?: number, end?: number): T[];
+export function addRange<T>(to: T[] | undefined, from: readonly T[] | undefined, start?: number, end?: number): T[] | undefined;
+export function addRange<T>(to: T[] | undefined, from: readonly T[] | undefined, start?: number, end?: number): T[] | undefined {
+    if (from === undefined || from.length === 0) return to;
+    if (to === undefined) return from.slice(start, end);
+    start = start === undefined ? 0 : toOffset(from, start);
+    end = end === undefined ? from.length : toOffset(from, end);
+    for (let i = start; i < end && i < from.length; i++) {
+        if (from[i] !== undefined) {
+            to.push(from[i]);
+        }
+    }
+    return to;
+}
+
+/**
+ * Returns the element at a specific offset in an array if non-empty, `undefined` otherwise.
+ * A negative offset indicates the element should be retrieved from the end of the array.
+ */
+export function elementAt<T>(array: readonly T[] | undefined, offset: number): T | undefined {
+    if (array) {
+        offset = toOffset(array, offset);
+        if (offset < array.length) {
+            return array[offset];
+        }
+    }
+    return undefined;
+}
